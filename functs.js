335d0e4fb1a8d4cf0e08701a929018fe6b9148ac
DenisBomugisha <dbomugisha@Rwanda.cmu.edu>
2017-03-11 15:21:23 +0200

    Comment:

--- functs.js
+++ functs.js
@@ -98,7 +98,7 @@ sendGenericMessage: function(toId)
               title: "Help Me Apply",
               payload: "help me apply",
             }]
-            },[{
+            ,{
             title: "Bot Party",
             subtitle: "Bots for Messenger Challenge",
             item_url: "https://messengerchallenge.splashthat.com/",
@@ -114,7 +114,7 @@ sendGenericMessage: function(toId)
               payload: "Payload for Please do something for me bubble",
             }],
             }]
-        
+        }
       }
     }
   };
